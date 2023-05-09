f4b9245ee7438a653b1371071f4e5b676725f1b5
IlyaPalatov <121305312+IlyaPalatov@users.noreply.github.com>
2023-05-09 16:18:05 +0300

    delete-all

--- js/task-05.js
+++
@@ -1,8 +0,0 @@
-const inputRef = document.querySelector("#name-input");
-const outputRef = document.querySelector("#name-output");
-
-inputRef.addEventListener("input", (event) => {
-    outputRef.textContent = event.target.value.trim() || "Anonymous";
-});
-
-
